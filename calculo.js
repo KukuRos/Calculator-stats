5e340775215e2705cf90deeb6bff3a745b033fe6
KukuRos <micaela.m.rosa@gmail.com>
2022-03-21 22:15:49 -0300

    Technics by stat F added

--- calculo.js
+++ calculo.js
@@ -48,5 +48,60 @@ function CalcSpellDice(){
 
 
 function CalcTechDice(){
-    
+    const statF = Number(document.getElementById("fuerza").value);
+    const statR = Number(document.getElementById("resistencia").value);
+    const statA = Number(document.getElementById("agilidad").value);
+    const statP = Number(document.getElementById("percepcion").value);
+    const statI = Number(document.getElementById("inteligencia").value);
+    const techResult = document.getElementById("QttyTechDice");
+    const NomResultT = document.getElementById("NomTechDice");
+
+
+    switch (document.getElementById("InputTechnics").value) {
+        case "GolpePesado":
+            var result = statF + 1;
+            techResult.innerText = ` Cantidad de dados: ${result}`;
+            NomResultT.innerText = `${result}d10`;
+
+            break;
+        case "Guillotina":
+            var result = statF + (statF/2);
+            techResult.innerText = ` Cantidad de dados: ${result}`;
+            NomResultT.innerText = `${result}d10`;
+            break;
+        case "RompeFilas":
+            var result = statF/2;
+            techResult.innerText = ` Dados: ${result}, enemigos: ${result}`;
+            NomResultT.innerText = `${result}d10`;
+            break;
+        case "MartilloCincel":
+            var result = statF + 0;
+            var result2 = statF/2
+            techResult.innerText = ` Enemigo A: ${result}. Enemigo B: ${result2}`;
+            NomResultT.innerText = `Tirada A: ${result}d10. Tirada B: ${result2}d10`;
+            break;
+        case "Nekodamashi":
+            var result = statF/2;
+            techResult.innerText = ` Cantidad de dados: ${result}`;
+            NomResultT.innerText = `${result}d10`;
+            break;
+        case "Burbaj":
+            var result = stat -1;
+            techResult.innerText = ` Cantidad de dados: ${result}`;
+            NomResultT.innerText = `${result}d10`;
+            break;
+        default:
+            techResult.innerText = `No válido`;
+            break;
+    }
+}
+
+function EmptyCSD(){
+    document.getElementById("QttySpellDice").innerHTML = "";
+    document.getElementById("NomSpellDice").innerHTML = "";
+}
+
+function EmptyCTD(){
+    document.getElementById("QttyTechDice").innerHTML = "";
+    document.getElementById("NomTechDice").innerHTML = "";
 }
